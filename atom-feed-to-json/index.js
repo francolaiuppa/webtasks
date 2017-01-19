989dcbbd469dd75b030d20227f11266b361ef7e9
Franco Laiuppa <franco.laiuppa@gmail.com>
2017-01-19 17:43:00 +0100

    Added atom-feed-to-json webtask

---
+++ atom-feed-to-json/index.js
@@ -0,0 +1,17 @@
+const REQUEST = require('request');
+const FEED = require('feed-read');
+
+module.exports = function(ctx, cb) {
+  if (!ctx.data.url) {
+    return cb(new Error('Missing `URL` querystring parameter'));
+  }
+
+  REQUEST.get(ctx.data.url, function(e, r) {
+    try {
+      FEED.atom(r.body, cb);
+    }
+    catch (e) {
+      cb(new Error(e));
+    }
+  });
+};
